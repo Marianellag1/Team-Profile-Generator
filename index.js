e42fc8f02ff3ee03840ef308c40e0fb356d3c7c6
Mgamboa <marianellag400@gmail.com>
2022-09-25 14:04:45 -0500

    made all js files under lib folder, all test files which where provided by professor and ta's are under tests, started index.js

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
 const inquirer = require('inquirer');
 
-inquirer
-.prompt([
-    {
-        type: 'input',
-        name: 'manager',
-        message: `What is the Team Manager's name?`,
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: `What is the Team Manager's employee ID?`,
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: `What is the Team Manager's email?`,
-    },
-    {
-        type: 'input',
-        name: 'office',
-        message: `What is the Team Manager's office number?`,
-    },
-    {
-        type: 'list',
-        name: 'menu',
-        message: `Which type of team member would you like to add?`,
-        choices: ['Engineer', 'Intern', 'Finish building my team']
-    },
-])
+const inquirer = require('inquirer');
+
+init()
+    .then(ans => {
+        console.log(ans)
+        if (ans.username === "test") {
+           takePassword()
+            .then(ans => console.log(ans))
+        }
+        
+    }
+    )
+console.log("hello");
+
+
+function init(){
+    return inquirer
+    .prompt(
+        [
+            {
+                type: "input",
+                message: "what is your name?",
+                name: "username"
+            }
+        ]
+    )
+}
+
+function takePassword() {
+   return inquirer
+        .prompt(
+            [
+                {
+                    type: "input",
+                    message: "what is your password?",
+                    name: "password"
+                }
+            ]
+        )
+}
+// inquirer
+// .prompt([
+//     {
+//         type: 'input',
+//         name: 'manager',
+//         message: `What is the Team Manager's name?`,
+//     },
+//     {
+//         type: 'input',
+//         name: 'id',
+//         message: `What is the Team Manager's employee ID?`,
+//     },
+//     {
+//         type: 'input',
+//         name: 'email',
+//         message: `What is the Team Manager's email?`,
+//     },
+//     {
+//         type: 'input',
+//         name: 'office',
+//         message: `What is the Team Manager's office number?`,
+//     },
+//     {
+//         type: 'list',
+//         name: 'menu',
+//         message: `Which type of team member would you like to add?`,
+//         choices: ['Engineer', 'Intern', 'Finish building my team']
+//     },
+// ])
