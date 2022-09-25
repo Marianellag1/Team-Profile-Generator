6d5820354fbdddab9dd4ad254b4c8c5b57401e9d
Mgamboa <marianellag400@gmail.com>
2022-09-25 17:30:11 -0500

    edited all files in lib folder, working on index.js for layout of questions with prompt

--- index.js
+++ index.js
@@ -1,71 +1,79 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/Employee');
 
-const inquirer = require('inquirer');
 
 init()
-    .then(ans => {
-        console.log(ans)
-        if (ans.username === "test") {
-           takePassword()
-            .then(ans => console.log(ans))
+    .then(employee => {
+        switch (employee) {
+            case "Manager":
+                addManager();
+                break;
+
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+            default://write here for html
+                break;
         }
-        
+
     }
     )
 console.log("hello");
 
 
-function init(){
+function init() {
     return inquirer
-    .prompt(
-        [
-            {
-                type: "input",
-                message: "what is your name?",
-                name: "username"
-            }
-        ]
-    )
-}
-
-function takePassword() {
-   return inquirer
         .prompt(
-            [
-                {
-                    type: "input",
-                    message: "what is your password?",
-                    name: "password"
-                }
-            ]
+            {
+                type: 'input',
+                name: 'manager',
+                message: `What is the Team Manager's name?`,
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: `What is the Team Manager's employee ID?`,
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: `What is the Team Manager's email?`,
+            },
+            {
+                type: 'input',
+                name: 'office',
+                message: `What is the Team Manager's office number?`,
+            },
+            {
+                type: 'list',
+                name: 'menu',
+                message: `Which type of team member would you like to add?`,
+                choices: ['Engineer', 'Intern', 'Finish building my team']
+            },
+
         )
 }
+
+// function takePassword() {
+//    return inquirer
+//         .prompt(
+//             [
+//                 {
+//                     type: "input",
+//                     message: "what is your password?",
+//                     name: "password"
+//                 }
+//             ]
+//         )
+// }
+
 // inquirer
 // .prompt([
-//     {
-//         type: 'input',
-//         name: 'manager',
-//         message: `What is the Team Manager's name?`,
-//     },
-//     {
-//         type: 'input',
-//         name: 'id',
-//         message: `What is the Team Manager's employee ID?`,
-//     },
-//     {
-//         type: 'input',
-//         name: 'email',
-//         message: `What is the Team Manager's email?`,
-//     },
-//     {
-//         type: 'input',
-//         name: 'office',
-//         message: `What is the Team Manager's office number?`,
-//     },
-//     {
-//         type: 'list',
-//         name: 'menu',
-//         message: `Which type of team member would you like to add?`,
-//         choices: ['Engineer', 'Intern', 'Finish building my team']
-//     },
 // ])
