309c6ecfba343173543a22599ed6846a66d3ace8
Mgamboa <marianellag400@gmail.com>
2022-09-25 19:00:56 -0500

    finished on team managers prompt, will now work on menu for choices of either engineer or intern

--- index.js
+++ index.js
@@ -3,76 +3,68 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/Employee');
 
 
-init()
-    .then(employee => {
-        switch (employee) {
-            case "Manager":
-                addManager();
-                break;
 
-            case "Engineer":
-                addEngineer();
-                break;
-            case "Intern":
-                addIntern();
-                break;
-            default://write here for html
-                break;
+const teamArray = [];
+
+const addManager = () => {
+        return inquirer
+            .prompt([   
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: `What is the Team Manager's name?`,
+                },
+                {
+                    type: 'input',
+                    name: 'id',
+                    message: `What is the Team Manager's employee ID?`,
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: `What is the Team Manager's email?`,
+                },
+                {
+                    type: 'input',
+                    name: 'office',
+                    message: `What is the Team Manager's office number?`,
+                },
+                {
+                    type: 'list',
+                    name: 'menu',
+                    message: `Which type of team member would you like to add?`,
+                    choices: ['Engineer', 'Intern', 'Finish building my team']
+                },
+            ]) .then(ans => {
+                console.log(ans);
+                const manager = new Manager (ans.name, ans.id, ans.email, ans.office);
+                teamArray.push(manager);
+                employeeMenu();
+            });
+
+
+                
+            // .then(employee => {
+            //     switch (employee.userChoice) {
+    
+            //         case "Engineer":
+            //             addEngineer();
+            //             break;
+            //         case "Intern":
+            //             addIntern();
+            //             break;
+            //         default://write here for html
+            //             break;
+            //     }
+            // })
         }
+    
+    
 
-    }
-    )
-console.log("hello");
-
-
-function init() {
-    return inquirer
-        .prompt(
-            {
-                type: 'input',
-                name: 'manager',
-                message: `What is the Team Manager's name?`,
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: `What is the Team Manager's employee ID?`,
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: `What is the Team Manager's email?`,
-            },
-            {
-                type: 'input',
-                name: 'office',
-                message: `What is the Team Manager's office number?`,
-            },
-            {
-                type: 'list',
-                name: 'menu',
-                message: `Which type of team member would you like to add?`,
-                choices: ['Engineer', 'Intern', 'Finish building my team']
-            },
-
-        )
-}
-
-// function takePassword() {
-//    return inquirer
-//         .prompt(
-//             [
-//                 {
-//                     type: "input",
-//                     message: "what is your password?",
-//                     name: "password"
-//                 }
-//             ]
-//         )
-// }
+//  function addEngineer()
+//  function addIntern()
 
 // inquirer
 // .prompt([
